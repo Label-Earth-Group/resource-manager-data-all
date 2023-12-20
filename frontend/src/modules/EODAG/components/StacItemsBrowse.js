da29d8c038d56fd7d72343e2e8876d507f541e72
no0o0oname <113674671@qq.com>
2023-12-20 20:52:51 +0800

    some unfinished changes for items browse

--- frontend/src/modules/EODAG/components/StacItemsBrowse.js
+++ frontend/src/modules/EODAG/components/StacItemsBrowse.js
@@ -1,52 +1,83 @@
 import { Link, Box, Card, CircularProgress } from '@mui/material';
 import { Link as RouterLink } from 'react-router-dom';
-import { useGetCollectionItemsByCollectionIDQuery } from '../services/eodagApi.ts';
-import { SET_ERROR, useDispatch } from 'globalErrors';
-import { useEffect } from 'react';
+import {
+  useGetCollectionItemsByCollectionIDQuery,
+  getCollectionQueryablesByCollectionID
+} from '../services/eodagApi.ts';
+import { useDispatch } from 'globalErrors';
+import { useHandleError } from '../utils.js';
 
 export function StacItemsBrowse(props) {
   const { collectionID } = props;
   const dispatch = useDispatch();
   const {
+    data: queryables,
+    errorQueryable,
+    isLoadingQueryable
+  } = getCollectionQueryablesByCollectionID(collectionID);
+  useHandleError(errorQueryable, dispatch);
+
+  const {
     data: items,
     error,
     isLoading
   } = useGetCollectionItemsByCollectionIDQuery({ collectionID });
+  useHandleError(error, dispatch);
 
-  useEffect(() => {
-    if (error) {
-      // Update state or dispatch action here
-      console.error(error);
-      dispatch({ type: SET_ERROR, error: error.error });
-      return <p>ERROR</p>;
-    }
-  }, [error, dispatch]);
+  if (errorQueryable || error) {
+    return <>Error</>;
+  }
 
-  if (isLoading) {
+  if (isLoadingQueryable || isLoading) {
     return <CircularProgress />;
   }
 
   if (!items) {
     return <></>;
   }
+  
+  queryables && {
+    // get the filter options from the queried collections
+    const filterOptions = Object.entries(EODAG_SUMMARY_INDEX).map(
+      ([filterName, pos]) => (
+        <Grid item md={2} sm={4} xs={12}>
+          <Autocomplete
+            id={filterName}
+            fullWidth
+            options={getSummaryFilters(collections, pos)}
+            renderInput={(params) => <TextField {...params} label={filterName} />}
+            onChange={handleFilterChange(filterName)}
+          ></Autocomplete>
+        </Grid>
+      )
+    );
+
+  }
 
   const { links, features, ...rest } = items;
 
   return (
-    <Box>
-      <Card sx={{ mb: 3 }}>{JSON.stringify(rest)}</Card>
-      {features.map((feature) => {
-        return (
-          <Card key={feature.id} sx={{ mb: 3, p: 2 }}>
-            <Link
-              component={RouterLink}
-              to={`/console/eodag/collections/${collectionID}/item/${feature.id}`}
-            >
-              {feature.id}
-            </Link>
-          </Card>
-        );
-      })}
-    </Box>
+    <>
+      
+
+      }
+      {items && (
+        <Box>
+          <Card sx={{ mb: 3 }}>{JSON.stringify(rest)}</Card>
+          {features.map((feature) => {
+            return (
+              <Card key={feature.id} sx={{ mb: 3, p: 2 }}>
+                <Link
+                  component={RouterLink}
+                  to={`/console/eodag/collections/${collectionID}/item/${feature.id}`}
+                >
+                  {feature.id}
+                </Link>
+              </Card>
+            );
+          })}
+        </Box>
+      )}
+    </>
   );
 }
