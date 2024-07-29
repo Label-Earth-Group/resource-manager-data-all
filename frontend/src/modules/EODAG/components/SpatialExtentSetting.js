0948283ba35014bb65ac012efd9826057c49fc63
Bo <1136743671@qq.com>
2024-07-29 21:15:14 +0800

    Further adjustment of file upload

--- frontend/src/modules/EODAG/components/SpatialExtentSetting.js
+++ frontend/src/modules/EODAG/components/SpatialExtentSetting.js
@@ -3,36 +3,93 @@ import { Button, Box, Typography, Tabs, Tab } from '@mui/material';
 import React, { useState, useEffect, useMemo, useRef } from 'react';
 import { useSettings } from 'design';
 import shp from 'shpjs';
+import * as toGeoJSON from 'togeojson';
+// import { GeoPackageAPI } from '@ngageoint/geopackage';
 
-const FileUploader = (setGeoJsonData) => {
-  const handleFileUpload = async (event) => {
-    const file = event.target.files[0];
-    if (file) {
+// async function parseGPKG(arrayBuffer) {
+//   const geoPackage = await GeoPackageAPI.open(arrayBuffer);
+//   const tables = await geoPackage.getFeatureTables();
+//   let geojson = { type: 'FeatureCollection', features: [] };
+
+//   for (let table of tables) {
+//     const featureDao = geoPackage.getFeatureDao(table);
+//     const iterator = featureDao.queryForAll();
+//     for (let row of iterator) {
+//       const feature = featureDao.getFeatureRow(row);
+//       geojson.features.push(feature.geometry.toGeoJSON());
+//     }
+//   }
+//   return geojson;
+// }
+
+const FileUploader = ({ setGeoJsonData }) => {
+  const handleShapefileUpload = async (files) => {
+    const fileMap = {};
+
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
       const reader = new FileReader();
-      reader.onload = async (e) => {
+
+      reader.onload = (e) => {
         const arrayBuffer = e.target.result;
-        let geojson;
+        fileMap[file.name] = arrayBuffer;
 
-        if (file.name.endsWith('.shp')) {
-          geojson = await shp(arrayBuffer);
-          console.log('geojson', geojson);
+        if (Object.keys(fileMap).length === files.length) {
+          const shpFiles = {};
+          Object.keys(fileMap).forEach((fileName) => {
+            const extension = fileName.split('.').pop();
+            shpFiles[extension] = fileMap[fileName];
+          });
+
+          if (shpFiles.shp) {
+            shp(shpFiles).then((result) => setGeoJsonData(result));
+          }
         }
-        // else if (file.name.endsWith('.kml')) {
-        //   geojson = parseKML(
-        //     new DOMParser().parseFromString(e.target.result, 'text/xml')
-        //   );
-        // } else if (file.name.endsWith('.gpkg')) {
-        //   geojson = await parseGPKG(arrayBuffer);
-        // }
-
-        setGeoJsonData(geojson);
       };
+
       reader.readAsArrayBuffer(file);
-      //   if (file.name.endsWith('.kml')) {
-      //     reader.readAsText(file);
-      //   } else {
-      //     reader.readAsArrayBuffer(file);
+    }
+  };
+
+  const handleSingleFileUpload = async (file) => {
+    const reader = new FileReader();
+    reader.onload = async (e) => {
+      const arrayBuffer = e.target.result;
+      let geojson;
+
+      if (file.name.endsWith('.kml')) {
+        geojson = toGeoJSON.kml(
+          new DOMParser().parseFromString(e.target.result, 'text/xml')
+        );
+      }
+      //   else if (file.name.endsWith('.gpkg')) {
+      //     geojson = await parseGPKG(arrayBuffer);
       //   }
+      else if (file.name.endsWith('.zip')) {
+        geojson = await shp(arrayBuffer);
+      }
+
+      setGeoJsonData(geojson);
+    };
+
+    if (file.name.endsWith('.kml')) {
+      reader.readAsText(file);
+    } else {
+      reader.readAsArrayBuffer(file);
+    }
+  };
+
+  const handleFileUpload = (event) => {
+    const files = event.target.files;
+    const shpFileExtensions = ['.shp', '.dbf', '.prj', '.cpg'];
+
+    if (
+      files.length > 1 ||
+      shpFileExtensions.some((ext) => files[0].name.endsWith(ext))
+    ) {
+      handleShapefileUpload(files);
+    } else {
+      handleSingleFileUpload(files[0]);
     }
   };
   return (
@@ -45,6 +102,12 @@ const FileUploader = (setGeoJsonData) => {
 export const SpatialExtentSetting = ({ setSpatialExtent }) => {
   const { settings } = useSettings();
   const [currentTab, setCurrentTab] = useState('区域绘制');
+  const [storedSpatialData, setStoredSpatialData] = useState({
+    drawn: null,
+    selected: null,
+    uploaded: null
+  });
+  console.info('storedSpatialData', storedSpatialData);
   const tabs = ['区域绘制', '行政区选择', '文件导入'];
   return (
     <>
@@ -72,7 +135,14 @@ export const SpatialExtentSetting = ({ setSpatialExtent }) => {
         })}
       </Tabs>
       {currentTab === '文件导入' && (
-        <FileUploader setGeoJsonData={setSpatialExtent} />
+        <FileUploader
+          setGeoJsonData={(geojson) => {
+            setStoredSpatialData({
+              ...storedSpatialData,
+              uploaded: geojson
+            });
+          }}
+        />
       )}
     </>
   );
